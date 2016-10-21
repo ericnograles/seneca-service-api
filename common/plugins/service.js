baa2f66f15129c7fb595dbf53ee0f6079388517c
Eric Nograles <ENograles@352inc.com>
2016-10-21 07:09:06 -0400

    adds tcp microservice support

--- common/plugins/service.js
+++ common/plugins/service.js
@@ -9,7 +9,8 @@ module.exports = function serviceRoutes() {
     var params = msg.args.params[0].split('/');
 
     if (params.length > 2) {
-      done(null, {error: 'Unrecognized path'});
+      msg.response$.status(500);
+      done(null, {message: 'Unrecognized path'});
     } else {
       // Convention: the role is the first parameter, the command is the second
       // If there is no command specified, we send the payload to the "index" command of the role in question
@@ -17,48 +18,62 @@ module.exports = function serviceRoutes() {
       var cmd = params.length > 1 ? params[1] : 'index';
       var pin = `role:${role},cmd:${cmd}`;
       var clientProtocol = msg.request$.query.protocol || 'amqp';
+      var clientConfig = {
+        pin: pin,
+        type: clientProtocol
+      };
+      var clientLibrary = 'seneca-transport';
 
-      var client = require('seneca')()
-          .use('seneca-amqp-transport')
-          .client({
-            type: clientProtocol,
-            pin: pin,
-            url: process.env.AMQP_URL || 'amqp://127.0.0.1'
-          });
+      if (clientProtocol === 'amqp') {
+        clientConfig.url = process.env.AMQP_URL || 'amqp://127.0.0.1';
+        clientLibrary = 'seneca-amqp-transport';
+      } else if (clientProtocol === 'tcp') {
+        clientConfig.port = msg.request$.query.port || 30301
+      }
 
-      client.act(pin, {
-        method: msg.request$.method,
-        query: msg.request$.query,
-        data: body.data
-      }, (err, res) => {
-        // Warning: not doing a client.close() leaves the response queue hanging
-        client.close();
+      try {
+        // Configure the client
+        var client = require('seneca')()
+          .use(clientLibrary)
+          .client(clientConfig);
 
-        // FYI, done(err) will bring down the entire process, use with care
-        if (err || res.error) {
-          var statusCode;
-          var errorPayload = {};
+        client.act(pin, {
+          method: msg.request$.method,
+          query: msg.request$.query,
+          data: body.data
+        }, (err, res) => {
+          // Warning: not doing a client.close() leaves the response queue hanging
+          client.close();
 
-          // Sanitized error outputs
-          if (err) {
-            errorPayload.message = err.msg;
-          } else if (res.error) {
-            errorPayload.message = res.error.message;
-          }
+          // FYI, done(err) will bring down the entire process, use with care
+          if (err || res.error) {
+            var statusCode;
+            var errorPayload = {};
 
-          if (err) {
-            statusCode = 500;
+            // Sanitized error outputs
+            if (err) {
+              errorPayload.message = err.msg;
+            } else if (res.error) {
+              errorPayload.message = res.error.message;
+            }
+
+            if (err) {
+              statusCode = 500;
+            } else {
+              statusCode = res.statusCode;
+            }
+
+            // Set the proper status code for HTTP
+            msg.response$.status(statusCode);
+            done(null, errorPayload);
           } else {
-            statusCode = res.statusCode;
+            done(null, res);
           }
-
-          // Set the proper status code for HTTP
-          msg.response$.status(statusCode);
-          done(null, errorPayload);
-        } else {
-          done(null, res);
-        }
-      });
+        });
+      } catch (err) {
+        msg.response$.status(500);
+        done(null, err);
+      }
     }
   });
 };
