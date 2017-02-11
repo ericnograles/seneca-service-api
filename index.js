825ef4eb3c8e3b7e43491e013fedd2fa53d6a428
Eric Nograles <ENograles@352inc.com>
2017-02-11 08:13:59 -0500

    refactors vars to const

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 'use strict';
 
-var Seneca = require('seneca');
-var Express = require('express');
-var Web = require('seneca-web');
-var BodyParser = require('body-parser');
-var Routes = require('./common/routes');
-var APIPlugin = require('./plugins/api');
-var UserPlugin = require('./plugins/user');
+const Seneca = require('seneca');
+const Express = require('express');
+const Web = require('seneca-web');
+const BodyParser = require('body-parser');
+const Routes = require('./common/routes');
+const APIPlugin = require('./plugins/api');
+const UserPlugin = require('./plugins/user');
 
-var app = Express();
+const app = Express();
 app.use(BodyParser.json({ type: 'application/*json' }));
 app.use(BodyParser.urlencoded({extended: true}));
 
 // TODO: Add Passport support and point handler to our OAuth handler
 
-var config = {
+const config = {
   routes: Routes,
   adapter: require('seneca-web-adapter-express'),
   context: app
 };
 
-var seneca = Seneca()
+const seneca = Seneca()
   .use(APIPlugin)
   .use(UserPlugin)
   .use(Web, config)
   .ready(() => {
-    var server = seneca.export('web/context')();
-    var port = process.env.PORT || 3001;
+    const server = seneca.export('web/context')();
+    const port = process.env.PORT || 3001;
     server.listen(port, () => {
       console.log(`server started on: ${port}`);
     })
